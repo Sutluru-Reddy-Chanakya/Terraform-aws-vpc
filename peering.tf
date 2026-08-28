fc343782dc0a1f5f993b3ace219dc35443a365c9
chanakya <you@example.com>
2026-08-28 14:53:35 +0530

    terraform

--- peering.tf
+++ peering.tf
@@ -39,7 +39,7 @@ resource "aws_route" "public_peering" {
 resource "aws_route" "private_peering" {
     count = var.is_peering_required ? 1:0
   route_table_id            = aws_route_table.private.id
-  destination_cidr_block    = var.vpc_cidr
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
@@ -47,7 +47,7 @@ resource "aws_route" "private_peering" {
 resource "aws_route" "db_peering" {
     count = var.is_peering_required ? 1:0
   route_table_id            = aws_route_table.db.id
-  destination_cidr_block    = var.vpc_cidr
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
