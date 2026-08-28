1945670886613a41972f6a64edc911850856e19a
chanakya <you@example.com>
2026-08-28 12:29:56 +0530

    terraform

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@ resource "aws_route_table" "db" {
 resource "aws_route" "public" {
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.gw.id
+  gateway_id = aws_internet_gateway.main.id
 }
 
 resource "aws_eip" "nat" {
