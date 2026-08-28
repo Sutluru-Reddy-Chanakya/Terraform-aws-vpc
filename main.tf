0ba31ad4b8472ffc3081eda827b1f5bcb3d2ef03
chanakya <you@example.com>
2026-08-28 12:33:27 +0530

    terraform

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ resource "aws_eip" "nat" {
 
 
 
-resource "aws_nat_gateway" "nat" {
+resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public[0].id                # we are creating in us-east -1a AZ
 
