66c81d653be2a4e5456ad0354eddb5b375791b3a
chanakya <you@example.com>
2026-08-28 12:12:15 +0530

    terraform

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@ resource "aws_route_table" "db" {
 resource "aws_route" "public" {
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.main.id
+  gateway_id = aws_internet_gateway.gw.id
 }
 
 resource "aws_eip" "nat" {
@@ -128,7 +128,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].nat.id                # we are creating in us-east -1a AZ
+  subnet_id     = aws_subnet.public[0].id                # we are creating in us-east -1a AZ
 
   tags =  merge(
           local.common_tags , {
