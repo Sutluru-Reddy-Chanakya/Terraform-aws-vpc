14263cb520025b322978da7db6e41fd8824e09e9
chanakya <you@example.com>
2026-08-28 11:18:00 +0530

    terraform

--- var.tf
+++ var.tf
@@ -26,7 +26,7 @@ variable "igw_tags" {
 }
 
 variable "public_subnet" {
-    type = list
+
     default = ["10.0.1.0/24" , "10.0.2.0/24"]
 }
 
@@ -38,7 +38,7 @@ variable "public_subnet_tags" {
 
 
 variable "private_subnet" {
-    type = list
+
     default = ["10.0.11.0/24" , "10.0.12.0/24"]
 }
 
@@ -50,12 +50,12 @@ variable "private_subnet_tags" {
 
 
 variable "db_subnet" {
-    type = list
+    
     default = ["10.0.21.0/24" , "10.0.22.0/24"]
 }
 
 variable "private_db_tags" {
-    type = map
+    
     default = {}
   
 }
@@ -64,7 +64,7 @@ variable "private_db_tags" {
 
 
 variable "public_route_table_tags" {
-    type = map
+    
     default = {}
   
 }
